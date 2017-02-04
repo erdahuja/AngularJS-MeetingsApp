a60d09288c277ed982985a972ddf65e2e94c3892
Deepak <er.d.ahuja@gmail.com>
2017-02-04 23:03:35 +0530

    gifts / awards functionality added

--- js/controllers/checkins.js
+++ js/controllers/checkins.js
@@ -1,51 +1,71 @@
-myApp.controller('CheckInsCtrl', ['$scope', '$firebaseArray', '$firebaseObject', '$rootScope', '$routeParams', '$location', function ($scope, $firebaseArray, $firebaseObject, $rootScope, $routeParams, $location) {
+myApp.controller('CheckInsCtrl', ['$scope', '$rootScope', '$location', '$routeParams', '$firebaseObject', '$firebaseArray',
+  function ($scope, $rootScope, $location, $routeParams, $firebaseObject, $firebaseArray) {
 
-    var ref;
+        var ref, checkinsList;
 
-    $scope.whichmeeting = $routeParams.mId;
-    $scope.whichuser = $routeParams.uId;
-    ref = firebase.database().ref().child('users').child($scope.whichuser).child('meetings').child($scope.whichmeeting).child('checkins');
+        $scope.whichmeeting = $routeParams.mId;
+        $scope.whichuser = $routeParams.uId;
 
-    checkinsList = $firebaseArray(ref);
-    $scope.checkins = checkinsList;
-    $scope.order = 'firstname';
-    $scope.direction = null;
-    $scope.query = '';
-    $scope.pickRandom = function () {
-        var rec = Math.round(Math.random() * (checkinsList.length - 1));
-        $scope.recordId = checkinsList.$keyAt(rec);
+        ref = firebase.database().ref()
+            .child('users').child($scope.whichuser)
+            .child('meetings').child($scope.whichmeeting)
+            .child('checkins');
 
-    };
-    $scope.showLove = function () {
+        checkinsList = $firebaseArray(ref);
+        $scope.checkins = checkinsList;
 
+        $scope.order = 'firstname';
+        $scope.direction = null;
+        $scope.query = '';
+        $scope.recordId = '';
 
-    };
+        $scope.pickRandom = function () {
+                var whichRecord = Math.round(Math.random() * (checkinsList.length - 1));
+                $scope.recordId = checkinsList.$keyAt(whichRecord);
+            } // pick a random winner
 
-    $scope.deleteCheckin = function (key) {
-        var refDel = ref.child(key);
-        var record = $firebaseObject(refDel);
-        record.$remove(key);
+        $scope.showLove = function (myCheckin) {
+            myCheckin.show = !myCheckin.show;
 
-    };
+            if (myCheckin.userState == 'expanded') {
+                myCheckin.userState = '';
+            } else {
+                myCheckin.userState = 'expanded';
+            }
+        }
 
-    $scope.addCheckin = function () {
+        $scope.giveLove = function (myCheckin, myGift) {
+            var refLove = ref.child(myCheckin.$id).child('awards');
+            var checkinsArray = $firebaseArray(refLove);
 
+            checkinsArray.$add({
+                name: myGift,
+                date: firebase.database.ServerValue.TIMESTAMP,
+            });
+        }
 
-        $firebaseArray(ref).$add({
-            firstname: $scope.user.firstname,
+        $scope.deleteLove = function (myCheckin, key) {
+            var refLove = ref.child(myCheckin.$id).child('awards').child(key);
+            var record = $firebaseObject(refLove);
+            record.$remove(key);
+        }
 
-            lastname: $scope.user.lastname,
-            date: firebase.database.ServerValue.TIMESTAMP,
-            email: $scope.user.email
+        $scope.addCheckin = function () {
+                $firebaseArray(ref).$add({
+                    firstname: $scope.user.firstname,
+                    lastname: $scope.user.lastname,
+                    email: $scope.user.email,
+                    date: firebase.database.ServerValue.TIMESTAMP
+                }).then(function () {
+                    $location.path('/checkins/' + $scope.whichuser + '/' +
+                        $scope.whichmeeting + '/checkinsList')
+                }); //$add
+            } //addCheckin
 
+        $scope.deleteCheckin = function (id) {
+            var refDel = ref.child(id);
+            var record = $firebaseObject(refDel);
+            record.$remove(id);
+        }
 
-        }).then(function () {
-
-            $location.path('/checkins' + '/' + $scope.whichuser + '/' + $scope.whichmeeting + '/' + 'checkinsList');
-
-        });
-    }
-
-
-
-}]);
+}]); //myApp.controller
